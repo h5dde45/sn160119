4ea75cc02848a5dfb70929d72a522b800d628b27
tmvf <tmvf@yandex.ru>
2019-01-16 16:47:05 +0400

    01

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import com.example.demo.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 4;
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("text", "text_1");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("text", "text_2");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("text", "text_3");
+        }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    @PostMapping()
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id,
+                                      @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+        messageFromDb.put("text", message.get("text"));
+        return message;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> messageFromDb = getMessage(id);
+        messages.remove(messageFromDb);
+    }
+}
