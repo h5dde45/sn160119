f9d816bc7c717dc5cc3944c24e831839dc188d4e
tmvf <tmvf@yandex.ru>
2019-01-19 11:26:38 +0400

    06

--- src/main/resources/static/js/components/messages/MessageList.vue
+++ src/main/resources/static/js/components/messages/MessageList.vue
@@ -1,14 +1,14 @@
 <template>
-    <div style="position: relative; width: 300px;">
+    <v-layout align-space-around justify-start column>
         <message-form :messages="messages" :messageAttr="message"></message-form>
-        <message-row v-for="message in messages"
+        <message-row v-for="message in sortedMessages"
                      :key="message.id"
                      :message="message"
                      :messages="messages"
                      :editMessage="editMessage"
                      :deleteMessage="deleteMessage"
         ></message-row>
-    </div>
+    </v-layout>
 </template>
 <script>
     import MessageRow from "./MessageRow.vue"
@@ -21,6 +21,11 @@
                 message: null
             }
         },
+        computed: {
+            sortedMessages(){
+                return this.messages.sort((a, b) => b.id - a.id)
+            }
+        },
         methods: {
             editMessage(message){
                 this.message = message
@@ -28,13 +33,13 @@
             deleteMessage(message){
                 this.$resource('/message{/id}')
                     .remove({id: message.id}).then(result => {
-                        if(result.ok){
-                            this.messages.splice(this.messages.indexOf(message),1)
+                    if (result.ok) {
+                        this.messages.splice(this.messages.indexOf(message), 1)
                     }
                 })
             }
         },
-        components:{
+        components: {
             MessageRow,
             MessageForm
         }
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>
-        <div v-if="!profile">Авторизация через <a href="/login">Google</a></div>
-        <div v-else>
-            <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
-            <messages-list :messages="messages"></messages-list>
-        </div>
-    </div>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title>SN</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <span v-if="profile">{{profile.name}}</span>
+            <v-btn v-if="profile" icon href="/logout">
+                <v-icon>
+                    directions_run
+                </v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <v-container v-if="!profile">Авторизация через <a href="/login">Google</a></v-container>
+            <v-container>
+                <messages-list v-if="profile" :messages="messages"></messages-list>
+            </v-container>
+        </v-content>
+
+    </v-app>
 </template>
 <script>
     import MessagesList from '../components/messages/MessageList.vue'
@@ -22,9 +34,9 @@
         created(){
             addHandler(data => {
                 let index = getIndex(this.messages, data.id);
-                if(index>-1){
+                if (index > -1) {
                     this.messages.splice(index, 1, data)
-                }else {
+                } else {
                     this.messages.push(data)
                 }
             })
@@ -35,5 +47,4 @@
     }
 </script>
 <style>
-
 </style>
